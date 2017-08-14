5d413d18b0464ec7d26974b6100b5efaa4420509
Unknown <zackbaldwin148@gmail.com>
2017-08-14 17:19:15 -0400

    1st
    1st

---
+++ app.js
@@ -0,0 +1,19 @@
+var express =require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+server.listen(8000)
+//var Redis = require('ioredis');
+//var redis = new Redis();
+var nunjucks =require('nunjucks')
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+})
+
+app.use(express.static('assets'))
+app.get('/', home)
+
+function home(req, res) {
+  res.render('index.html')
+}
