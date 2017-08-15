3b55a95a4a78778f986d034fd7a65bc19f66c1e7
Unknown <zackbaldwin148@gmail.com>
2017-08-14 22:23:26 -0400

    Revamped
    fixed html, css, fonts etc

--- app.js
+++ app.js
@@ -17,19 +17,27 @@ app.get('/ipas', ipas)
 app.get('/signed', signed)
 app.get('/betas', betas)
 app.get('/jailbreaks', jailbreaks)
+app.get('/upload', upload)
+app.get('/credits', credits)
 
 function home(req, res) {
-  res.render('index.html')
+  res.render('index.html', {title: 'iOS Haven'})
 }
 function ipas(req, res) {
-  res.render('ipas.html')
+  res.render('ipas.html', {title: 'IPA Library - iOS Haven'})
 }
 function signed(req, res) {
-  res.render('signed.html')
+  res.render('signed.html', {title: 'Signed IPAs - iOS Haven'})
 }
 function betas(req, res) {
-  res.render('betas.html')
+  res.render('betas.html', {title: 'Beta Downloads - iOS Haven '})
 }
 function jailbreaks(req, res) {
-  res.render('jailbreaks.html')
+  res.render('jailbreaks.html', {title: 'iOS Jailbreaks - iOS Haven'})
+}
+function upload(req, res) {
+  res.render('upload.html', {title: 'Upload an IPA - iOS Haven'})
+}
+function credits(req, res) {
+  res.render('credits.html', {title: 'Credits - iOS Haven'})
 }
