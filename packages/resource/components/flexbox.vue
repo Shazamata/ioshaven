3f9d2317cc85aefa20c83022864459ee20eb34f4
Unknown <zackbaldwin148@gmail.com>
2017-08-19 00:36:30 -0400

    vue
    vue

---
+++ packages/resource/components/flexbox.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container-2-box">
+      <h4 class="center">{{title}}</h4>
+      <img :src='image' class="center-block"/>
+      <br>
+      <p class="center">{{version}}</p>
+      <p class="height">{{desc}}</p>
+      <a :href="dl" class="btn btn-primary center-block">Download Here</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "image", "version", "desc", "dl"],
+  mounted(){
+  }
+}
+</script>
+
+<style lang="css">
+img {
+  cursor: default;
+  pointer-events: none;
+  width: 192px;
+  height: 192px;
+}
+
+.center {
+  text-align: center;
+}
+
+.height {
+  height: 200px;
+  overflow-y: auto;
+}
+
+.center-block {
+  display: block;
+  margin: 0 auto;
+}
+
+.container-2 {
+  display: flex;
+  justify-content: center;
+}
+
+.container-2-box {
+  width: 20%;
+  border: 1px #ffffff solid;
+  padding: 20px 25px;
+  margin: 40px;
+}
+</style>
--- packages/resource/js.js
+++ packages/resource/js.js
@@ -1,1 +1,12 @@
-require('bootstrap')
+//require('popper')
+//require('bootstrap')
+window.Vue = require('vue')
+
+
+Vue.component('flexbox', require('./components/flexbox.vue'))
+const app = new Vue({
+  el: '#app',
+  data: {
+    apps: require('./sideload-apps.json')
+  }
+})
